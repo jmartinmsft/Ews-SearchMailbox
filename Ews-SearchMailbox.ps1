8130ad8f920c5e3b54943f0bdf2f7eb5c68c54e7
jmartinmsft <72947063+jmartinmsft@users.noreply.github.com>
2022-12-02 09:25:09 -0500

    Bug fix
    WellKnownFolder always connected to primary. This is fixed.

--- Ews-SearchMailbox.ps1
+++ Ews-SearchMailbox.ps1
@@ -57,7 +57,7 @@ function Enable-TraceHandler(){
 function Get-OAuthToken{
     #Change the AppId, AppSecret, and TenantId to match your registered application
     $AppId = "6a93c8c4-9cf6-4efe-a8ab-9eb178b8dff4"
-    $AppSecret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    $AppSecret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     $TenantId = "9101fc97-5be5-4438-a1d7-83e051e52057"
     #Build the URI for the token request
     $Uri = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
@@ -205,19 +205,19 @@ if($EnableLogging) {
 
 #Find-MailboxItem -FolderName $FolderName -MailboxName $MailboxName -EwsService $service -OutputFile $CsvFile -MeetingSubject $Subject
 try{
-    $folderid = New-Object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::$FolderName,$MailboxName)
+        if($Archive) {
+            $folderid = new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::ArchiveMsgFolderRoot,$MailboxName)
+        }
+        else {
+            $folderid = new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::MsgFolderRoot,$MailboxName)
+        }
 }
 catch{
-#$rootContainer = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid)
-    if($Archive) {
-        $folderid = new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::ArchiveMsgFolderRoot,$MailboxName)
-    }
-    else {
-        $folderid = new-object Microsoft.Exchange.WebServices.Data.FolderId([Microsoft.Exchange.WebServices.Data.WellKnownFolderName]::MsgFolderRoot,$MailboxName)
-    }
+    Write-Warning "Unable to connect to the mailbox for $MailboxName."
+    exit
 }
 $rootContainer = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderid)  
-if($FolderName -notin $WellKnownFolderNames) {
+if($FolderName -notin $WellKnownFolderNames -or $Archive) {
     $fvFolderView = New-Object Microsoft.Exchange.WebServices.Data.FolderView(1)
     $SfSearchFilter = New-Object Microsoft.Exchange.WebServices.Data.SearchFilter+IsEqualTo([Microsoft.Exchange.WebServices.Data.FolderSchema]::DisplayName,$FolderName)
     $findFolderResults = $service.FindFolders($rootContainer.Id,$SfSearchFilter,$fvFolderView)
